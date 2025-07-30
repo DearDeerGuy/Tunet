f57e488b0592d19ec5cbc24b36785a728848c345
Haruyuki <goodthefly2003@gmail.com>
2025-07-31 01:23:39 +0300

    Changes to FileController

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -11,7 +11,37 @@ use Illuminate\Http\Request;
 class FileController extends Controller
 {
     public function index(Request $request) {
+        $validated = $request->validate([
+            'film_id' => 'required|exists:films,id',
+        ]);
 
+        $filmId = $validated['film_id'];
+        $film = Film::with('files')->find($filmId);
+        
+        if ($film->type === 'film') {
+            $file = $film->files->first();
+
+            if ($file)
+                $result = ['link' => $file->link];
+            else
+                $result = [];
+        }
+        else
+            // Для сериала возвращаем 'season_number' и 'episode_number'
+            $result = $film->files
+                ->sortBy([
+                    ['season_number', 'asc'],
+                    ['episode_number', 'asc'],
+                    ])
+                ->values()
+                ->map(function ($file) {
+                    return [
+                        'season_number' => $file->season_number,
+                        'episode_number' => $file->episode_number,
+                    ];
+                });
+
+        return response()->json($result);
     }
     public function getSerialInfo(Films $film) {
         if($film->type!="serial")
@@ -25,6 +55,11 @@ class FileController extends Controller
     public function store(FileRequest $request) {
         $validated = $request->validated();
         
+        // Проверяем, есть ли уже файлы у фильма
+        $existingFilesCount = Files::where('films_id', $validated['film_id'])->count();
+        if ($existingFilesCount > 0) 
+            return response()->json(['message' => 'Файлы для этого фильма уже загружены.'], 409);
+
         $file = $request->file('file');
         $filename = uniqid() . '.' . $file->getClientOriginalExtension();
         $path = $file->storeAs('films', $filename, 'public');
