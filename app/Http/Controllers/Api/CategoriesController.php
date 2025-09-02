8781e3eb06ea67d918767b98a2230b36491eee2d
Aist023 <lololo228505aist@gmail.com>
2025-09-03 00:42:05 +0300

    Category update

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -11,17 +11,12 @@ class CategoriesController extends Controller
 {
     public function index()
     {
-        $perPage = request()->get('per_page', 10);
-        return response()->json(
-            Categories::paginate($perPage)
-        );
+        return response()->json(Categories::all());
     }
 
     public function store(CreateCategoryRequest $request)
     {
         $validated = $request->validated();
-
-        Log::channel('admin')->alert("");
         $category = Categories::create($validated);
         return response()->json($category, 201);
     }
@@ -34,16 +29,13 @@ class CategoriesController extends Controller
     public function update(CreateCategoryRequest $request, Categories $category)
     {
         $validated = $request->validated();
-
         $category->update($validated);
-        Log::channel('admin')->alert("");
         return response()->json($category);
     }
 
     public function destroy(Categories $category)
     {
         $category->delete();
-        Log::channel('admin')->alert("");
         return response()->json(null, 204);
     }
 }
